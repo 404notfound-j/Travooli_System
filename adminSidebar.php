876918795b35e000d1445b8dcc61eb547a87dfde
Edwin Chua <edwinchua922@gmail.com>
2025-07-03 14:48:34 +0800

    refund button

--- adminSidebar.php
+++ adminSidebar.php
@@ -71,6 +71,7 @@ $initials = strtoupper(substr($adminData['fst_name'], 0, 1) . substr($adminData[
             <a href="#" class="nav-item">Booking</a>
             <a href="#" class="nav-item">Report</a>
             <a href="#" class="nav-item">Payment</a>
+            <a href="#" class="nav-item">Refund</a>
             <a href="#" class="nav-item">User Management</a>
             
             <div class="pages-section">
