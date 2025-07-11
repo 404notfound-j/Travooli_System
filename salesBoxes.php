39b5dcda2ce1ef4c6c705feff1b7408a3d621f88
Kwun01 <kahwunchan05@gmail.com>
2025-07-11 14:17:43 +0800

    ABC

--- salesBoxes.php
+++ salesBoxes.php
@@ -5,7 +5,11 @@ if (!isset($items) || !is_array($items))return;
     <?php foreach ($items as $item): ?>
         <div class="airline-box">
             <div class="airline-box-top">
-                <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['name']; ?>" class="airline-logo">
+                <?php
+                //flight-logo for flight images, .airline-logo for hotel images
+                $imgClass = (isset($item['image']) && strpos($item['image'], 'images/') === 0 && substr($item['image'], -4) === '.png') ? 'flight-logo' : 'airline-logo';
+                ?>
+                <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['name']; ?>" class="<?php echo $imgClass; ?>">
             </div>
             <div class="airline-box-middle">
                 <div class="airline-name"><?php echo $item['name']; ?></div>
